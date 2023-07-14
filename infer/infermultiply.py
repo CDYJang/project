6319c60ffd3e019a3a3d173bdd85f493dec3f850
edberg <edberg@gmail.com>
2023-07-14 15:20:21 +0900

    first commit

---
+++ infer/infermultiply.py
@@ -0,0 +1,45 @@
+import os
+from flask import Flask, request, jsonify
+import sys
+path=os.environ.get('LIBPATH')
+path='./lib'
+sys.path.append(path)
+
+from multiply import multi_numbers_from_file
+
+sys.path.append('./infer')
+
+# WHAT TO DO
+# 1. 추론과 관련없는 것은 다 날렸습니다.
+# 2. 추론을 위한 코드를 추가했습니다.
+# 3. 모델을 읽어오기 위한 코드를 추가했습니다. (모델 패쓰 포함)
+# 4. Flask를 통한 POST 서비스를 위한 코드를 추가했습니다.
+
+app = Flask(__name__)                   #4
+app.debug = True                        #4
+
+@app.route('/')
+def hello_world():
+    file_path = './data/data.txt'
+    file= open(file_path, 'r')
+    a=file.readlines()
+    sen='Data example \n'
+    for i in a:
+        sen+=i
+    return sen
+
+@app.route('/multiply', methods=['POST'])          #4
+def multiply():                                  #4
+    if 'data' not in request.files:                #4
+        return "No Data file uploaded", 400        #4
+    data_file = request.files['data']             #4
+    try:                                            #4
+        result = multi_numbers_from_file(data_file)
+        print("1")
+        return "숫자의 총 곱셈:"+ str(result)
+
+    except Exception as e:                              #4
+        return f"Error recognizing data: {str(e)}", 500    #4
+
+if __name__ == '__main__':      #4
+    app.run(host='0.0.0.0',port=5000)                   #4
---
+++ lib/multiply.py
@@ -0,0 +1,17 @@
+def multi_numbers_from_file(file):
+    total = 1
+    for line in file:
+        try:
+            number = float(line)
+            total *= number
+        except ValueError:
+            # 예외 처리: 숫자로 변환할 수 없는 줄은 건너뜁니다.
+            continue
+    return total
+
+# 텍스트 파일 경로를 지정하여 함수를 호출합니다.
+if __name__ == '__main__':
+    file_path = './data/data.txt'  # 실제 파일 경로로 변경해야 합니다.
+    with open(file_path, 'r') as file:
+        result = multi_numbers_from_file(file_path)
+    print("숫자의 총 곱셈:", result)
